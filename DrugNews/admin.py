ca8cffe14e9d6a4e7512151ff36fcf408480dd7a
peiwen0809 <candy86080925@gmail.com>
2021-12-15 22:29:13 +0800

    211215-001 修改新聞模組的URL

--- DrugNews/admin.py
+++ DrugNews/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import NewsList
+from DrugNews import models
 # Register your models.
-admin.site.register(NewsList)
+admin.site.register(models.NewsList)
